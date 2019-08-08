ea2222dd538dd11a12c29ee46547c6a6d502c9e3
ziyuan <hanziyuan08@outlook.com>
2019-08-08 12:05:34 +0800

    Some features and some improvements
    1. Support export specified blogs.
    2. Camel Case to Underscore

--- README.md
+++ README.md
@@ -1,31 +1,33 @@
 # csdn-export
-export csdn blog and create github issue blog.
+导出CSDN博客并创建对应的Github Issue博客。  
+通过本工具你既可以一次性导出所有博客，也可以指定某一篇要导出的博客。
 
-# Usage
-`python csdn-export.py --help` for details.
+# 使用
+`python csdn-export.py --help`查看详细。
 
-## Premise
+## 前提
 ```
 $ pip install pipenv
 $ pipenv install
 ```
 
-## Demo
+## 例子
 ```
 $ echo CSDN_COOKIE > cookie.txt 
-$ pipenv run python csdn-export.py -u GITHUB_USERNAME -r GITHUB_REPO -t GITHUB_ACCESS_TOKEN -p CSDN_BLOG_MAX_PAGE -c CSDN_USERNAME
+$ pipenv run python csdn-export.py -u GITHUB_USERNAME -r GITHUB_REPO -t GITHUB_ACCESS_TOKEN -p CSDN_BLOG_MAX_PAGE -c CSDN_USERNAME -b BLOG_ID
 ```
 
 ## Variables
-`GITHUB_ACCESS_TOKEN`: Generate under here, https://github.com/settings/tokens  
-`GITHUB_USERNAME`: Your Github username.  
-`GITHUB_REPO`: Your Github repository.  
-`CSDN_BLOG_MAX_PAGE`: Your CSDN blog max page. It exists because the program cannot get the maximum page number now.   
-`CSDN_COOKIE`: CSDN cookie. You can find it in CSDN Ajax.  
-`CSDN_USERNAME`: CSDN username.  
+`GITHUB_ACCESS_TOKEN`: 在后面的网址中生成：https://github.com/settings/tokens  
+`GITHUB_USERNAME`: 你的Github用户名  
+`GITHUB_REPO`: 你的Github仓库名  
+`CSDN_BLOG_MAX_PAGE`: 你的CSDN博客最大页码. 存在这个变量的原因是目前程序无法获取最大页码     
+`CSDN_COOKIE`: 你的CSDN cookie，你可以在控制台中复制随便一个csdn请求的cookie。  
+`CSDN_USERNAME`: 你的CSDN用户名  
+`BLOG_ID`: 你的CSDN博客ID  
 
 
-# Warning
-DO NOT USE THE TOOL FOR OTHERS REPO!!!  
-DO NOT USE THE TOOL FOR OTHERS REPO!!!  
-DO NOT USE THE TOOL FOR OTHERS REPO!!!  
+# 警告
+不要对他人的仓库使用本工具!!!  
+不要对他人的仓库使用本工具!!!  
+不要对他人的仓库使用本工具!!!  
--- csdn-export.py
+++ csdn-export.py
@@ -34,12 +34,13 @@ def make_sure_create_issue(title, body, token, username, repo, times=0):
     return created
 
 
-baseCsdn = 'https://blog.csdn.net/{username}//article/list/{page}'
-baseCsdnMarkdown = 'https://mp.csdn.net/mdeditor/getArticle?id={articleId}'
+base_csdn = 'https://blog.csdn.net/{username}/article/list/{page}'
+base_csdn_detail = 'https://blog.csdn.net/{username}/article/details/{blog_id}'
+base_csdn_markdown = 'https://mp.csdn.net/mdeditor/getArticle?id={articleId}'
 
 
 def get_csdn_page(username):
-    url = baseCsdn.replace('{username}', username).replace('{page}', '1')
+    url = base_csdn.replace('{username}', username).replace('{page}', '1')
     print(requests.get(url).text)
     soup = BeautifulSoup(requests.get(url).text, 'html.parser')
     page_htmls = soup.select('li[data-page]')
@@ -48,7 +49,7 @@ def get_csdn_page(username):
 
 
 def query_csdn_article_urls(page_num, username):
-    url = baseCsdn.replace('{username}', username).replace('{page}', str(page_num))
+    url = base_csdn.replace('{username}', username).replace('{page}', str(page_num))
     soup = BeautifulSoup(requests.get(url).text, 'html.parser')
     article_htmls = soup.select('h4 > a')
 
@@ -88,7 +89,7 @@ def get_csdn_markdown(article_id):
         'user-agent': 'Mozilla/ 5.0(Windows; NT; 10.0;Win64;x64) AppleWebKit / 537.36(KHTML, like; Gecko) Chrome / '
                       '73.0; .3683; .103; Safari / 537.36',
     }
-    r = requests.get(baseCsdnMarkdown.replace('{articleId}', article_id), headers=headers)
+    r = requests.get(base_csdn_markdown.replace('{articleId}', article_id), headers=headers)
     ret = json.loads(r.text)
     if ret['error']:
         raise CsdnException('%s 请检查Cookie是否设置, article_id: %s' % (ret['error'], article_id))
@@ -116,23 +117,30 @@ if __name__ == '__main__':
     parser.add_argument('-p', '--page', type=int, help="csdn blog max page")
     parser.add_argument('-u', '--username', help="github account username")
     parser.add_argument('-c', '--csdnusername', help="csdn account username")
+    parser.add_argument('-b', '--csdnblogids', help="csdn blog ids", nargs='*')
     parser.add_argument('-r', '--repo', help="github account repo")
     args = parser.parse_args()
 
-    allArticleUrls = []
-    # 取出所有问文章
-    for page in range(1, args.page + 1):
-        articleUrls = query_csdn_article_urls(page, args.csdnusername)
-        for articleUrl in articleUrls:
-            allArticleUrls.append(articleUrl)
+    all_article_urls = []
+    # 取出所有文章
+    if args.page:
+        for page in range(1, args.page + 1):
+            for articleUrl in query_csdn_article_urls(page, args.csdnusername):
+                all_article_urls.append(articleUrl)
+
+    # 指定文章ID
+    if args.csdnblogids:
+        for blog_id in args.csdnblogids:
+            all_article_urls.append(
+                base_csdn_detail.replace('{username}', args.csdnusername).replace('{blog_id}', blog_id))
 
     # 按照文章发表时间创建issue，最早的文章先创建issue
-    for articleUrl in reversed(allArticleUrls):
-        articleId = extract_csdn_article_id(articleUrl)
-        markdownArticle = get_csdn_markdown(articleId)
-        print(markdownArticle)
+    for articleUrl in reversed(all_article_urls):
+        article_id = extract_csdn_article_id(articleUrl)
+        markdown_article = get_csdn_markdown(article_id)
+        print(markdown_article)
         # save_file(markdownArticle['title'] + '.md', markdownArticle['markdowncontent'])
-        success = make_sure_create_issue(markdownArticle['title'], markdownArticle['markdowncontent'], args.token,
+        success = make_sure_create_issue(markdown_article['title'], markdown_article['markdowncontent'], args.token,
                                          args.username, args.repo)
         if not success:
             print('Article migration failed: %s' % articleUrl)
